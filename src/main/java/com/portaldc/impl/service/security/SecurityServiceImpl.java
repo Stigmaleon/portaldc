c4f04e5e004bdd29a66ca28c33050604f65e2b33
stigmaleon <stigmaleon@gmail.com>
2014-07-29 11:46:57 +0300

    *fix errors in setting of spring

--- src/main/java/com/portaldc/impl/service/security/SecurityServiceImpl.java
+++ src/main/java/com/portaldc/impl/service/security/SecurityServiceImpl.java
@@ -2,7 +2,8 @@ package com.portaldc.impl.service.security;
 
 import com.portaldc.api.dao.UserDAO;
 import com.portaldc.api.model.User;
-import com.portaldc.impl.dao.UserDAOImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
