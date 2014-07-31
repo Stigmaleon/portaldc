ef174e7cac784438b106a589e2ca02787fbf4b3b
stigmaleon <stigmaleon@gmail.com>
2014-07-31 13:01:27 +0300

    *fix import

--- src/main/java/com/portaldc/web/RedirectController.java
+++ src/main/java/com/portaldc/web/RedirectController.java
@@ -1,13 +1,8 @@
 package com.portaldc.web;
 
-import com.portaldc.api.model.User;
-import com.portaldc.impl.service.security.SecurityServiceImpl;
-import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class RedirectController{
