14c065527cd6803cfb5e1d11a04942cef0bc9db0
Dzmitry Ramaniuk <stigmaleon@gmail.com>
2016-01-07 15:44:57 +0300

    dc-58 add localization

--- src/main/java/com/portaldc/config/AppConfig.java
+++ src/main/java/com/portaldc/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.portaldc.config;
 
+import com.portaldc.impl.service.message.MessageServiceImpl;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,16 +9,57 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.portaldc")
-public class AppConfig extends WebMvcConfigurerAdapter{
+public class AppConfig extends WebMvcConfigurerAdapter {
 
     @Bean
-    public MessageSource messageSource(){
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+
+        return interceptor;
+    }
+
+    @Bean
+    public CookieLocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("ru"));
+
+        return localeResolver;
+    }
+
+    @Bean
+    public RequestMappingHandlerMapping handlerMapping() {
+        RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
+        handlerMapping.setInterceptors(new Object[]{localeChangeInterceptor()});
+
+        return handlerMapping;
+    }
+
+    @Bean
+    public MessageServiceImpl messageService() throws UnsupportedEncodingException {
+        List<String> languages = new ArrayList<>();
+        languages.add("ru.json");
+        languages.add("en.json");
+
+        return new MessageServiceImpl(languages);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
+        messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("UTF-8");
 
         return messageSource;
