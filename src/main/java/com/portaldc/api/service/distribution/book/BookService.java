41fd8ca0a95271286fc0818be12d258b13251e17
Dzmitry Ramaniuk <dzmitry.ramaniuk@gmail.com>
2016-04-03 14:48:33 +0300

    dc-58 use jhipster progect structure

--- src/main/java/com/portaldc/api/service/distribution/book/BookService.java
+++
@@ -1,7 +0,0 @@
-package com.portaldc.api.service.distribution.book;
-
-import com.portaldc.api.dto.distibution.book.BookDTO;
-
-public interface BookService {
-    Long saveBook(BookDTO bookDTO);
-}
